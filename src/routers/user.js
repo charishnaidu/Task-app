fb1e0d018a2211b57426d1454dc90d3d4ea8c260
Chinnam <harish.chinnam@realtech.com>
2022-08-24 17:12:42 +0200

    Generating the authentication tokens

--- src/routers/user.js
+++ src/routers/user.js
@@ -4,10 +4,11 @@ const User = require('../models/user')
 const router = new express.Router()
 
 router.post('/users',  async (req, res)=>{
-    const user = new User(req.body) 
+    const user = new User(req.body)
     try{
         await user.save()
-        res.status(201).send(user)
+        const token = await user.generateAuthToken()
+        res.status(201).send({user, token})
     }catch(e){
         res.status(400).send(e)
     }
@@ -16,7 +17,8 @@ router.post('/users',  async (req, res)=>{
 router.post('/users/login',  async (req, res)=>{
     try{
         const user = await User.findByCredentials(req.body.email, req.body.password)
-        res.status(200).send(user)
+        const token = await user.generateAuthToken()
+        res.status(200).send({user, token})
     }catch(e){
         res.status(400).send()
     }
