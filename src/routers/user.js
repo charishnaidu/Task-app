19a5697160c603b893fb87c7d2d52d6cc24a55d7
Chinnam <harish.chinnam@realtech.com>
2022-08-30 09:30:11 +0200

    Handling express errors

--- src/routers/user.js
+++ src/routers/user.js
@@ -83,7 +83,7 @@ const upload = multer({
         fileSize : 1000000
     },
     fileFilter(req, file, cb){
-        if(!file.originalname.match(/\.(jpg |png |jpeg)$/)){
+        if(!file.originalname.match(/\.(jpg|png|jpeg)$/)){
             return cb(new Error('Please upload an image with size less than 1MB!!'))
         }
         cb(undefined, true)
@@ -92,6 +92,8 @@ const upload = multer({
 
 router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
     res.send()
+}, (error, req, res, next) =>{
+    res.status(400).send({error: error.message})
 })
 
 module.exports = router
