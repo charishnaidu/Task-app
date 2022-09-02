bc8ccdb883bb579a10273a284ff92c658a44725d
Chinnam <harish.chinnam@realtech.com>
2022-09-02 07:31:50 +0200

    Using the environment variables

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
 const app = express()
-const port = process.env.PORT || 4000
+const port = process.env.PORT
 
 app.use(express.json())
 app.use(userRouter)
